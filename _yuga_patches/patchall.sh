4709ff5914a0c039e0897a9f48c8236a6072c351
Adrian Ulrich <adrian@blinkenlights.ch>
2014-11-22 23:03:28 +0100

    Add patch to fix derp in userland Dear qcom: if you mess up your kernel headers, you should fix them and NOT mess up the userland also

--- _yuga_patches/patchall.sh
+++ _yuga_patches/patchall.sh
@@ -15,3 +15,4 @@ patch -d packages/apps/Browser -f -p1 < $CWD/02-browser.diff
 patch -d packages/apps/Nfc     -f -p1 < $CWD/03-nfc.diff
 patch -d packages/apps/Email   -f -p1 < $CWD/04-email.diff
 patch -d hardware/qcom/display -f -p1 < $CWD/05-qcom-display.diff
+patch -d hardware/qcom/media   -f -p1 < $CWD/06-qcom-media.diff
