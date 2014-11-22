f33f4743d6f003a572f9edf9a97cbe39d7895f48
Adrian Ulrich <adrian@blinkenlights.ch>
2014-11-22 22:06:22 +0100

    add patch for qcom display fail

--- _yuga_patches/patchall.sh
+++ _yuga_patches/patchall.sh
@@ -14,3 +14,4 @@ patch -d external/sepolicy     -f -p1 < $CWD/01-selinux.diff
 patch -d packages/apps/Browser -f -p1 < $CWD/02-browser.diff
 patch -d packages/apps/Nfc     -f -p1 < $CWD/03-nfc.diff
 patch -d packages/apps/Email   -f -p1 < $CWD/04-email.diff
+patch -d hardware/qcom/display -f -p1 < $CWD/05-qcom-display.diff
